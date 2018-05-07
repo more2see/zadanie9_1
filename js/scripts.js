bcdb3510b82778445e59aeae9dee0a88a1582028
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-07 14:54:50 +0200

    Change scirpts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
 function getTriangleArea(a, h) {
-	if (a <= 0) return 'nieprawidlowe dane';
-	else if (h <= 0) return 'nieprawidlowe dane';
-	else((a > 0) && (h > 0));
-	return (a * h / 2);
-};
+ if ((a <= 0) || (h <= 0)) {
+ return "nieprawidlowe dane";
+ } else if ((a > 0) && (h > 0)) {
+ var getTriangleArea = (a * h / 2)
+ return getTriangleArea;
+  }
+}
 console.log(getTriangleArea(10, 6));
 var triangle1Area = getTriangleArea(10, 15);
 console.log(triangle1Area);
@@ -11,3 +13,4 @@ var triangle2Area = getTriangleArea(-2, 20);
 console.log(triangle2Area);
 var triangle3Area = getTriangleArea(7, 3);
 console.log(triangle3Area);
+
