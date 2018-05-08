ab9494a146bb70fe6ccf7f3d2d26bd3af21f2d90
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-08 23:40:41 +0200

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-function Area(a, h) {
+function area(a, h) {
 	if ((a > 0) && (h > 0)) {
-		return a * h /2;
+		return a * h / 2;
 	} else {
 		return 'niepoprawne dane';
 	}
 }
-console.log(Area(10, 6));
-var triangle1Area = Area(10, 15);
+
+console.log(area(10, 6));
+
+var triangle1Area = area(10, 15);
 console.log(triangle1Area);
-var triangle2Area = Area(-2, 20);
+var triangle2Area = area(-2, 20);
 console.log(triangle2Area);
-var triangle3Area = Area(7, 3);
+var triangle3Area = area(7, 3);
 console.log(triangle3Area);
 
 
