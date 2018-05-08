b9e0034972c582cf2b74bd53e98450bac234ce14
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-08 20:27:05 +0200

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-function getTriangleArea(a, h) {
- if ((a <= 0) || (h <= 0)) {
- return "nieprawidlowe dane";
- } else if ((a > 0) && (h > 0)) {
- var getTriangleArea = (a * h / 2)
- return getTriangleArea;
-  }
+function Area(a, h) {
+	if ((a > 0) && (h > 0)) {
+		return a * h /2;
+	} else {
+		return 'niepoprawne dane';
+	}
 }
-console.log(getTriangleArea(10, 6));
-var triangle1Area = getTriangleArea(10, 15);
+console.log(Area(10, 6));
+var triangle1Area = Area(10, 15);
 console.log(triangle1Area);
-var triangle2Area = getTriangleArea(-2, 20);
+var triangle2Area = Area(-2, 20);
 console.log(triangle2Area);
-var triangle3Area = getTriangleArea(7, 3);
+var triangle3Area = Area(7, 3);
 console.log(triangle3Area);
 
+
+/* BYŁO, MOJE STARE: 
+function Area(a, h) {
+	if ((a <= 0) || (h <= 0)) {
+		return "nieprawidlowe dane";
+ 	} else if ((a > 0) && (h > 0)) {
+ 		var Area = a * h / 2;
+ 		return Area;
+ 	}
+}
+*/
